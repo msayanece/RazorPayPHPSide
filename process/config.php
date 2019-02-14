9ffa50eca11071e5ed1006f66c95f995103bf3eb
Sayan Mukherjee <msayanece@gmail.com>
2019-02-14 10:39:27 +0530

    update config.php
    update config.php change MID, secret key for security

--- process/config.php
+++ process/config.php
@@ -1,4 +1,4 @@
 <?php
-	define("MID", 					"rzp_test_93QmbCWVj8XHK7");
-	define("SECRET_KEY", 			"jNIhNb1eanG7poq5DHOPgldp");
+	define("MID", 					"rzp_test_93XXXXXXXXXHK7");
+	define("SECRET_KEY", 			"jNIhNb1eXXXXXXXXXldp");
 ?>
